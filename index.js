0706b0e1ed857b1cbbc3875248be96525a70b307
Brian Short <brian.ralph.short@gmail.com>
2020-11-25 09:13:53 -0500

    Add boilerplate with dependencies to access mysql, console.table, and inquirer

---
+++ index.js
@@ -0,0 +1,18 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "rootroot",
+  database: "employee_tracker_DB"
+});
+
